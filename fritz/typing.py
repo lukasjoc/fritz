defba1e0bd5ef37ee3378759f55c3b60dd2f5a39
lukasjoc <jochamlu@gmail.com>
2022-06-19 14:02:19 +0200

    implement info, reboot commands for FRITZ!Box
    - implement get info command to retrive information about the router
    - implement reboot command to reboot the router easily

---
+++ fritz/typing.py
@@ -0,0 +1,4 @@
+Config = dict[any]
+Path = str
+Port = str or int
+EXIT_SUCCESS = 0
---
+++ main.py
@@ -0,0 +1,57 @@
+from fritz.typing import EXIT_SUCCESS, Path
+from fritz.api import Router
+import argparse
+import sys
+
+def main(args) -> None:
+    if args.reboot:
+        router = Router(args.config)
+        connection = router.connect()
+        print(f"Rebooting {connection.modelname}@{router.connector.connection_host}...",)
+        connection.reboot()
+        print("Get a 🍵 and wait until the router is reachable again.")
+    elif args.info:
+        router = Router(args.config)
+        connection = router.connect()
+        connector  = router.connector
+        print("Model Details: ")
+        print()
+        print(f"Model:   {connection.modelname}")
+        print(f"Version: {connection.system_version}")
+        print()
+        print("Connection Details: ")
+        print(f"Host:    {connector.connection_host}")
+        print(f"Port:    {connector.connection_port}")
+        print(f"TLS:     {connector.connection_use_tls}")
+
+    sys.exit(EXIT_SUCCESS)
+
+if __name__ == "__main__":
+    def str2bool(v):
+        if isinstance(v, bool): return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'): return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'): return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
+    parser = argparse.ArgumentParser(description="FRITZ!Box (Simple) Interface")
+
+    parser.add_argument("--info", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="connect and show info about the router")
+
+    parser.add_argument("--reboot", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="connect and reboot the router")
+
+    parser.add_argument("--config", type=Path, default="config.yml",
+                        help="path to the configuration file")
+
+
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1.0",
+        help="print the current version",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
